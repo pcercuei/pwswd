b998b1bd6e01fa8d7ee60374bc749312b1e99e7b
Paul Cercueil <paul@voyager.(none)>
2011-02-01 18:30:01 +0100

    Rewrite of the brightness backend. The step size is now 10 (previously 1).

--- backend/brightness/brightness.c
+++ backend/brightness/brightness.c
@@ -10,42 +10,85 @@
 #define MAX_BRIGHT_FILENAME "/sys/class/backlight/pwm-backlight/max_brightness"
 #define BLANKING_FILENAME "/sys/class/graphics/fb0/blank"
 
+#define MIN_BRIGHTNESS 5
+#define STEP_VALUE 10
+
+static int max_brightness = 0;
+static int current_value = 0;
 static FILE *dev_file = NULL;
-static int max_brightness;
-static int current_value;
-static char tmp[0x10];
+
+static int get_current_value(FILE *dev)
+{
+	char tmp[0x10];
+
+	fread(tmp, sizeof(char), 0x10, dev);
+	return atoi(tmp);
+}
+
+static void set_brightness(FILE *dev, int value)
+{
+	char tmp[0x10];
+
+	sprintf(tmp, "%i", value);
+	write(fileno(dev), tmp, strlen(tmp)+1);
+}
 
 static void bright_init() {
-    // First: we retrieve the maximum brightness parameter.
-    dev_file = fopen(MAX_BRIGHT_FILENAME, "r");
-    fread(tmp, sizeof(char), 0x10, dev_file);
-    max_brightness = atoi(tmp);
-    fclose(dev_file);
+	// We retrieve the maximum brightness parameter.
 
-    // Second: we retrieve the current backlight power value.
-    dev_file = fopen(BRIGHT_FILENAME, "r+");
-    fread(tmp, sizeof(char), 0x10, dev_file);
-    current_value = atoi(tmp);
+	FILE *file;
+	char tmp[0x10];
+
+	file = fopen(MAX_BRIGHT_FILENAME, "r");
+	fread(tmp, sizeof(char), 0x10, file);
+	max_brightness = atoi(tmp);
+	fclose(file);
 }
 
 void bright_up(int event_value) {
-    if (!dev_file) bright_init();
+	if (!max_brightness) bright_init();
 
-    if (current_value == max_brightness)
-      return;
+	// If the button has been released, we simply close the BRIGHT_FILENAME file.
+	if (event_value == 0) {
+		fclose(dev_file);
+		return;
+	}
 
-    sprintf(tmp, "%i", ++current_value);
-//    printf("Value: %s.\n", tmp);
-    write(fileno(dev_file), tmp, strlen(tmp)+1);
+	// If the button has been pressed, update the current_value variable.
+	else if (event_value == 1) {
+		dev_file = fopen(BRIGHT_FILENAME, "r+");
+		current_value = get_current_value(dev_file);
+	}
+
+	if (current_value + STEP_VALUE >= max_brightness)
+	  current_value = max_brightness;
+
+	else
+	  current_value += STEP_VALUE;
+
+	set_brightness(dev_file, current_value);
 }
 
 void bright_down(int event_value) {
-    if (!dev_file) bright_init();
+	if (!max_brightness) bright_init();
 
-    if (current_value == 5)
-      return;
+	// If the button has been released, we simply close the BRIGHT_FILENAME file.
+	if (event_value == 0) {
+		fclose(dev_file);
+		return;
+	}
 
-    sprintf(tmp, "%i", --current_value);
-//    printf("Value: %s.\n", tmp);
-    write(fileno(dev_file), tmp, strlen(tmp)+1);
+	// If the button has been pressed, update the current_value variable.
+	else if (event_value == 1) {
+		dev_file = fopen(BRIGHT_FILENAME, "r+");
+		current_value = get_current_value(dev_file);
+	}
+
+	if (current_value - STEP_VALUE <= MIN_BRIGHTNESS)
+	  current_value = MIN_BRIGHTNESS;
+
+	else
+	  current_value -= STEP_VALUE;
+
+	set_brightness(dev_file, current_value);
 }
