2edd24077e13aa46c2d51998802bb806f1309984
Ayla <paul@crapouillou.net>
2011-03-06 15:02:47 +0100

    The TV-out backend has been rewritten. Using system(), now it will be able to launch a script instead of a binary.

--- backend/tvout/tvout.c
+++ backend/tvout/tvout.c
@@ -1,12 +1,8 @@
 
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 
 #include "tvout_backend.h"
-
-#define TVTOOL_BINARY "tvtool"
-#define ACTIVATE "--on"
-#define DISACTIVATE "--off"
 
 static int activated = 0;
 
@@ -14,11 +10,8 @@ void tv_out(void)
 {
 	activated = !activated;
 
-	if ( fork() ) {
-		// if to be activated
-		if (activated)
-			execlp(TVTOOL_BINARY, TVTOOL_BINARY, ACTIVATE, (char*)NULL);
-		else
-			execlp(TVTOOL_BINARY, TVTOOL_BINARY, DISACTIVATE, (char*)NULL);
-	}
+	if (activated)
+	  system("/usr/sbin/tvtool --on");
+	else
+	  system("/usr/sbin/tvtool --off");
 }
