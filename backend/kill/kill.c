7f39f4c90a98a853855752fc1f9dc442626f0c67
Ayla <paul@crapouillou.net>
2012-04-13 19:03:37 +0200

    Allow the daemon to kill console-based applications too.

--- backend/kill/kill.c
+++ backend/kill/kill.c
@@ -1,10 +1,25 @@
 
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "../backends.h"
 
+#define TTY1 "/dev/tty1"
+
 void do_kill(void)
 {
-	if (!fork())
+	int status;
+	pid_t son = fork();
+	if (!son)
 		execlp("fuser", "fuser", "-k", "-HUP", FRAMEBUFFER, NULL);
+
+	waitpid(son, &status, 0);
+	if (!status)
+		return;
+
+	/* Perhaps the framebuffer was not in use;
+	 * try to kill a console-based application. */
+	if (!fork())
+		execlp("fuser", "fuser", "-k", "-HUP", TTY1, NULL);
 }
