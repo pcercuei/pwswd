pcercuei/pwswd

commit c70ae27f50f31aba858e040524c6f10094ff5b46

Fix backends executed twice, at key press and key release events

event_listener.c
@@ -105,14 +105,14 @@ static void execute(enum event_type event, int value)
 	switch(event) {
 #ifdef BACKEND_REBOOT
 		case reboot:
-			if (value == 2) return;
+			if (value != 1) return;
 			str = "reboot";
 			do_reboot();
 			break;
 #endif
 #ifdef BACKEND_POWEROFF
 		case poweroff:
-			if (value == 2) return;
+			if (value != 1) return;
 			str = "poweroff";
 			do_poweroff();
 			break;
@@ -125,7 +125,7 @@ static void execute(enum event_type event, int value)
 			break;
 #endif
 		case hold:
-			if (value == 2) return;
+			if (value != 1) return;
 			str = "hold";
 			if (mode == HOLD)
 			  switchmode(NORMAL);
@@ -153,7 +153,7 @@ static void execute(enum event_type event, int value)
 			break;
 #endif
 		case mouse:
-			if (value == 2) return;
+			if (value != 1) return;
 			str = "mouse";
 			if (mode == MOUSE)
 			  switchmode(NORMAL);
@@ -162,21 +162,21 @@ static void execute(enum event_type event, int value)
 			break;
 #ifdef BACKEND_TVOUT
 		case tvout:
-			if (value == 2) return;
+			if (value != 1) return;
 			str = "tvout";
 			tv_out();
 			break;
 #endif
 #ifdef BACKEND_SCREENSHOT
 		case screenshot:
-			if (value == 2) return;
+			if (value != 1) return;
 			str = "screenshot";
 			do_screenshot();
 			break;
 #endif
 #ifdef BACKEND_KILL
 		case kill:
-			if (value == 2) return;
+			if (value != 1) return;
 			str = "kill";
 			do_kill();
 			break;

commit 9ce22727bceedfb2dd6d258f6207e94851698c5b

Fixed poweroff by holding power slider
The poweroff backend should be notified of both press and release
events. This got broken in c70ae27f. Thanks to Paul for describing
the fix.

event_listener.c
@@ -113,7 +113,7 @@ static void execute(enum event_type event, int value)
 #endif
 #ifdef BACKEND_POWEROFF
 		case poweroff:
-			if (value != 1) return;
+			if (value == 2) return;
 			str = "poweroff";
 			do_poweroff();
 			break;